5c2b8ebc6349f86aca4b1b52eda821377efc1c8b
islam-24-03 <128147089+islam-24-03@users.noreply.github.com>
2023-03-28 23:56:49 +0600

    Add files via upload

---
+++ DZ7_islam_24-03/app.js
@@ -0,0 +1,95 @@
+const buttons = document.querySelectorAll('.show-answer')
+
+buttons.forEach(button => {
+    button.addEventListener("click" , () => {
+        const answer = button.nextElementSibling
+
+        if (answer.style.display === "none") {
+            answer.style.display = "block"
+            button.textContent = "Закрыть"
+        }else{
+            answer.style.display = "none"
+            button.textContent = "Ответ"
+        }
+    })
+})
+
+const buttons_1 = document.querySelectorAll('.show-answer_1')
+
+buttons_1.forEach(button => {
+    button.addEventListener("click" , () => {
+        const answer = button.nextElementSibling
+
+        if (answer.style.display === "none") {
+            answer.style.display = "block"
+            button.textContent = "Закрыть"
+        }else{
+            answer.style.display = "none"
+            button.textContent = "Ответ"
+        }
+    })
+})
+
+const buttons_2 = document.querySelectorAll('.show-answer_2')
+
+buttons_2.forEach(button => {
+    button.addEventListener("click" , () => {
+        const answer = button.nextElementSibling
+
+        if (answer.style.display === "none") {
+            answer.style.display = "block"
+            button.textContent = "Закрыть"
+        }else{
+            answer.style.display = "none"
+            button.textContent = "Ответ"
+        }
+    })
+})
+
+const buttons_3 = document.querySelectorAll('.show-answer_3')
+
+buttons_3.forEach(button => {
+    button.addEventListener("click" , () => {
+        const answer = button.nextElementSibling
+
+        if (answer.style.display === "none") {
+            answer.style.display = "block"
+            button.textContent = "Закрыть"
+        }else{
+            answer.style.display = "none"
+            button.textContent = "Ответ"
+        }
+    })
+})
+
+const buttons_4 = document.querySelectorAll('.show-answer_4')
+
+buttons_4.forEach(button => {
+    button.addEventListener("click" , () => {
+        const answer = button.nextElementSibling
+
+        if (answer.style.display === "none") {
+            answer.style.display = "block"
+            button.textContent = "Закрыть"
+        }else{
+            answer.style.display = "none"
+            button.textContent = "Ответ"
+        }
+    })
+})
+
+const buttons_5 = document.querySelectorAll('.show-answer_5')
+
+buttons_5.forEach(button => {
+    button.addEventListener("click" , () => {
+        const answer = button.nextElementSibling
+
+        if (answer.style.display === "none") {
+            answer.style.display = "block"
+            button.textContent = "Закрыть"
+        }else{
+            answer.style.display = "none"
+            button.textContent = "Ответ"
+        }
+    })
+})
